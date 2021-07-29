e8e5f5d9272ab9575095b14aac6b36f8578d5a36
Icey Chuigda WhiteGive <icey@icey.tech>
2021-07-29 09:15:47 +0800

    用一波海外的利空消息来砸盘

--- mock-server/index.js
+++ mock-server/index.js
@@ -64,19 +64,19 @@ app.get('/service/get_company_news', (req, res) => {
                         title: '上市公司X面临破产清算',
                         date: '2014年5月14日',
                         source: '野鸡新闻',
-                        emot: ''
+                        emot: -1
                     },
                     {
                         title: 'X公司股指逼近600点冲击新高',
                         date: '2014年3月12日',
                         source: '野鸡新闻',
-                        emot: ''
+                        emot: 1
                     },
                     {
                         title: 'X公司今日在深交所挂牌上市',
                         date: '2013年12月1日',
                         source: '野鸡新闻',
-                        emot: ''
+                        emot: 0
                     }
                 ]
             }
